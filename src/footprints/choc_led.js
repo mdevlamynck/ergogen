a575caedace768b4e6bdae344cfea87804834de8
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2022-10-16 15:22:09 +0200

    Add promicro pretty footprint

--- src/footprints/choc_led.js
+++ src/footprints/choc_led.js
@@ -7,16 +7,18 @@ module.exports = {
     VCC: 'VCC',
     GND: 'GND'
   },
-  params: {},
-  body: p => {
+  params: {
+    class: 'S'
+  },
+  body: p =>
     `
       (module Choc_Hotswap_SK6812MiniE (layer F.Cu) (tedit 610F112B)
-        ${p.at /* parametric position */}
+        ${p.at}
 
-        (fp_text reference SW4 (at 6.85 8.45 -180) (layer F.SilkS) hide
+        (fp_text reference SW4 (at 6.85 7.62 ${p.rot - 180}) (layer F.SilkS) hide
           (effects (font (size 1 1) (thickness 0.15)))
         )
-        (fp_text value SW_PUSH (at -4.95 8.6 -180) (layer F.Fab) hide
+        (fp_text value SW_PUSH (at -4.95 7.62 ${p.rot - 180}) (layer F.Fab) hide
           (effects (font (size 1 1) (thickness 0.15)))
         )
         (fp_line (start -4.4 7.05154) (end -4.4 5.40054) (layer F.SilkS) (width 0.12))
@@ -182,10 +184,10 @@ module.exports = {
         (fp_line (start 3.725 -1.375) (end 6.275 -1.375) (layer B.SilkS) (width 0.15))
         (fp_line (start -1.3 -3.575) (end 1.275 -3.575) (layer B.SilkS) (width 0.15))
         (fp_line (start -1.3 -8.225) (end 1.3 -8.225) (layer B.SilkS) (width 0.15))
-        (fp_line (start 9.525 9.525) (end -9.525 9.525) (layer Dwgs.User) (width 0.15))
-        (fp_line (start -9.525 9.525) (end -9.525 -9.525) (layer Dwgs.User) (width 0.15))
-        (fp_line (start -9.525 -9.525) (end 9.525 -9.525) (layer Dwgs.User) (width 0.15))
-        (fp_line (start 9.525 -9.525) (end 9.525 9.525) (layer Dwgs.User) (width 0.15))
+        (fp_line (start 8.72 8.225) (end -8.72 8.225) (layer Dwgs.User) (width 0.15))
+        (fp_line (start -8.72 8.225) (end -8.72 -8.225) (layer Dwgs.User) (width 0.15))
+        (fp_line (start -8.72 -8.225) (end 8.72 -8.225) (layer Dwgs.User) (width 0.15))
+        (fp_line (start 8.72 -8.225) (end 8.72 8.225) (layer Dwgs.User) (width 0.15))
         (fp_line (start 1.8 3.149) (end -1.8 3.149) (layer Edge.Cuts) (width 0.12))
         (fp_line (start 1.8 6.249) (end 1.8 3.149) (layer Edge.Cuts) (width 0.12))
         (fp_line (start -1.8 6.249) (end 1.8 6.249) (layer Edge.Cuts) (width 0.12))
@@ -219,29 +221,26 @@ module.exports = {
         (fp_arc (start -6.3 -5.025) (end -6.3 -6.025) (angle -90) (layer F.SilkS) (width 0.15))
         (fp_arc (start -6.3 -2.375) (end -6.3 -1.375) (angle 90) (layer F.SilkS) (width 0.15))
         (fp_arc (start -3.737801 -2.303096) (end -2.837801 -2.078096) (angle 73.56696737) (layer F.SilkS) (width 0.15))
-        (pad "" np_thru_hole circle (at -5.5 0 270) (size 1.8 1.8) (drill 1.8) (layers *.Cu *.Mask))
-        (pad "" np_thru_hole circle (at 5.5 0 270) (size 1.8 1.8) (drill 1.8) (layers *.Cu *.Mask))
-        (pad "" np_thru_hole circle (at 0 0 270) (size 3.4 3.4) (drill 3.4) (layers *.Cu *.Mask))
-        (pad "" np_thru_hole circle (at 0 -5.9 270) (size 3 3) (drill 3) (layers *.Cu *.Mask))
-        (pad "" np_thru_hole circle (at 5 -3.7 270) (size 3 3) (drill 3) (layers *.Cu *.Mask))
-        (pad 5 smd rect (at -2.8 -5.9) (size 1.6 2.2) (layers B.Cu B.Paste B.Mask) ${p.net.from.str})
-        (pad 6 smd rect (at 7.8 -3.7) (size 1.6 2.2) (layers B.Cu B.Paste B.Mask)
-          (clearance 0.3) ${p.net.to.str})
-        (pad 6 smd rect (at -7.9 -3.7) (size 1.6 2.2) (layers F.Cu F.Paste F.Mask)
-          (clearance 0.5) ${p.net.to.str})
-        (pad 5 smd rect (at 2.8 -5.9) (size 1.6 2.2) (layers F.Cu F.Paste F.Mask) ${p.net.from.str})
-        (pad "" np_thru_hole circle (at -5 -3.7 90) (size 3 3) (drill 3) (layers *.Cu *.Mask))
-        (pad "" np_thru_hole circle (at 5.22 4.2) (size 1 1) (drill 1) (layers *.Cu *.Mask))
-        (pad "" np_thru_hole circle (at -5.22 4.2) (size 1 1) (drill 1) (layers *.Cu *.Mask))
-        (pad 2 smd rect (at 2.75 5.399) (size 1.7 1) (layers F.Cu F.Paste F.Mask) ${p.net.din.str})
-        (pad 3 smd rect (at -2.75 5.399) (size 1.7 1) (layers F.Cu F.Paste F.Mask) ${p.net.GND.str})
-        (pad 1 smd rect (at 2.75 3.999) (size 1.7 1) (layers F.Cu F.Paste F.Mask) ${p.net.VCC.str})
-        (pad 4 smd rect (at -2.75 3.999) (size 1.7 1) (layers F.Cu F.Paste F.Mask) ${p.net.dout.str})
-        (pad 3 smd rect (at -2.75 3.999) (size 1.7 1) (layers B.Cu B.Paste B.Mask) ${p.net.GND.str})
-        (pad 2 smd rect (at 2.75 3.999) (size 1.7 1) (layers B.Cu B.Paste B.Mask) ${p.net.din.str})
-        (pad 4 smd rect (at -2.75 5.399) (size 1.7 1) (layers B.Cu B.Paste B.Mask) ${p.net.dout.str})
-        (pad 1 smd rect (at 2.75 5.399) (size 1.7 1) (layers B.Cu B.Paste B.Mask) ${p.net.VCC.str})
+        (pad "" np_thru_hole circle (at -5.5 0 ${p.rot + 270}) (size 1.8 1.8) (drill 1.8) (layers *.Cu *.Mask))
+        (pad "" np_thru_hole circle (at 5.5 0 ${p.rot + 270}) (size 1.8 1.8) (drill 1.8) (layers *.Cu *.Mask))
+        (pad "" np_thru_hole circle (at 0 0 ${p.rot + 270}) (size 3.4 3.4) (drill 3.4) (layers *.Cu *.Mask))
+        (pad "" np_thru_hole circle (at 0 -5.9 ${p.rot + 270}) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+        (pad "" np_thru_hole circle (at 5 -3.7 ${p.rot + 270}) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+        (pad 5 smd rect (at -2.8 -5.9 ${p.rot}) (size 1.6 2.2) (layers B.Cu B.Paste B.Mask) ${p.net.from.str})
+        (pad 6 smd rect (at 7.8 -3.7 ${p.rot}) (size 1.6 2.2) (layers B.Cu B.Paste B.Mask) (clearance 0.3) ${p.net.to.str})
+        (pad 6 smd rect (at -7.9 -3.7 ${p.rot}) (size 1.6 2.2) (layers F.Cu F.Paste F.Mask) (clearance 0.5) ${p.net.to.str})
+        (pad 5 smd rect (at 2.8 -5.9 ${p.rot}) (size 1.6 2.2) (layers F.Cu F.Paste F.Mask) ${p.net.from.str})
+        (pad "" np_thru_hole circle (at -5 -3.7 ${p.rot + 90}) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+        (pad "" np_thru_hole circle (at 5.22 4.2 ${p.rot}) (size 1 1) (drill 1) (layers *.Cu *.Mask))
+        (pad "" np_thru_hole circle (at -5.22 4.2 ${p.rot}) (size 1 1) (drill 1) (layers *.Cu *.Mask))
+        (pad 2 smd rect (at 2.75 5.399 ${p.rot}) (size 1.7 1) (layers F.Cu F.Paste F.Mask) ${p.net.dout.str})
+        (pad 3 smd rect (at -2.75 5.399 ${p.rot}) (size 1.7 1) (layers F.Cu F.Paste F.Mask) ${p.net.GND.str})
+        (pad 1 smd rect (at 2.75 3.999 ${p.rot}) (size 1.7 1) (layers F.Cu F.Paste F.Mask) ${p.net.VCC.str})
+        (pad 4 smd rect (at -2.75 3.999 ${p.rot}) (size 1.7 1) (layers F.Cu F.Paste F.Mask) ${p.net.din.str})
+        (pad 3 smd rect (at -2.75 3.999 ${p.rot}) (size 1.7 1) (layers B.Cu B.Paste B.Mask) ${p.net.GND.str})
+        (pad 2 smd rect (at 2.75 3.999 ${p.rot}) (size 1.7 1) (layers B.Cu B.Paste B.Mask) ${p.net.dout.str})
+        (pad 4 smd rect (at -2.75 5.399 ${p.rot}) (size 1.7 1) (layers B.Cu B.Paste B.Mask) ${p.net.din.str})
+        (pad 1 smd rect (at 2.75 5.399 ${p.rot}) (size 1.7 1) (layers B.Cu B.Paste B.Mask) ${p.net.VCC.str})
       )
     `
-  }
 }
--- src/footprints/index.js
+++ src/footprints/index.js
@@ -12,6 +12,7 @@ module.exports = {
     omron: require('./omron'),
     pad: require('./pad'),
     promicro: require('./promicro'),
+    promicro_pretty: require('./promicro_pretty'),
     rgb: require('./rgb'),
     rotary: require('./rotary'),
     scrollwheel: require('./scrollwheel'),
